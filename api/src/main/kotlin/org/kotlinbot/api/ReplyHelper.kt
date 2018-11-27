ee999560d599289ad32be087804fe241ab0aa95b
Alexandr Latushkin <alexandr.latushkin@gmail.com>
2018-11-27 17:33:40 +0300

    reply helper api refinement

--- api/src/main/kotlin/org/kotlinbot/api/ReplyHelper.kt
+++ api/src/main/kotlin/org/kotlinbot/api/ReplyHelper.kt
@@ -1,17 +1,18 @@
 package org.kotlinbot.api
 
 import org.kotlinbot.api.inevents.InEvent
+import org.kotlinbot.api.inevents.Text
 
 typealias ReplyCondition<T> = suspend (InEvent?) -> T?
 
 interface ReplyHelper {
-    suspend fun <T> on(predicate: ReplyCondition<T>, action: suspend (T) -> Unit)
+    suspend fun <T, E : InEvent> on(predicate: ReplyCondition<T>, action: suspend (event: E, T) -> Unit)
 
-    suspend fun onText(predicate: suspend (String) -> Boolean, action: suspend () -> Unit)
-    suspend fun <T> onText(predicate: suspend (String) -> T?, action: suspend (T) -> Unit)
-    suspend fun onText(regexpMatch: Regex, action: suspend (MatchResult) -> Unit)
-    suspend fun onText(vararg exact: String, action: suspend (String) -> Unit)
+    suspend fun onText(predicate: suspend (String) -> Boolean, action: suspend (textMsg: Text) -> Unit)
+    suspend fun <T> onText(predicate: suspend (String) -> T?, action: suspend (textMsg: Text, T) -> Unit)
+    suspend fun onText(regexpMatch: Regex, action: suspend (textMsg: Text, matches: MatchResult) -> Unit)
+    suspend fun onText(vararg exact: String, action: suspend (textMsg: Text, String) -> Unit)
+    suspend fun textOtherwise(action: suspend (textMsg: Text) -> Unit)
 
-    suspend fun textOtherwise(action: suspend () -> Unit)
     suspend fun otherwise(action: suspend () -> Unit)
 }
--- api/src/main/kotlin/org/kotlinbot/api/ReplyHelperImpl.kt
+++ api/src/main/kotlin/org/kotlinbot/api/ReplyHelperImpl.kt
@@ -10,29 +10,36 @@ data class ReplyHelperImpl(val callScope: BotScope, val inEvent: InEvent?) :
     private val msg = inEvent
 
 
-    override suspend fun onText(predicate: suspend (String) -> Boolean, action: suspend () -> Unit) {
-        if (!matched && msg is Text && (predicate(msg.message) || predicate(EmojiParser.removeAllEmojis(msg.message).trim())))
+    override suspend fun onText(predicate: suspend (String) -> Boolean, action: suspend (textMsg: Text) -> Unit) {
+        if (!matched && msg is Text && (
+                    predicate(msg.message)
+                            || predicate(EmojiParser.removeAllEmojis(msg.message).trim())
+                    )
+        )
             execute(action)
     }
 
 
-    override suspend fun <T> onText(predicate: suspend (String) -> T?, action: suspend (T) -> Unit) {
+    override suspend fun <T> onText(predicate: suspend (String) -> T?, action: suspend (textMsg: Text, T) -> Unit) {
         if (!matched && msg is Text)
             (predicate(msg.message)
                 ?: predicate(EmojiParser.removeAllEmojis(msg.message).trim()))?.let { execute(action, it) }
     }
 
 
-    override suspend fun onText(vararg exact: String, action: suspend (String) -> Unit) = onText({ msgText ->
+    override suspend fun onText(
+        vararg exact: String,
+        action: suspend (textMsg: Text, String) -> Unit
+    ) = onText({ msgText ->
         val onlyText = msgText.replace("[.,:;!?()<>\\[\\]{}]".toRegex(), "").trim()
         exact.find { it.equals(onlyText, true) }
     }, action)
 
-    override suspend fun onText(regexpMatch: Regex, action: suspend (MatchResult) -> Unit) =
+    override suspend fun onText(regexpMatch: Regex, action: suspend (textMsg: Text, matches: MatchResult) -> Unit) =
         onText({ regexpMatch.find(it) }, action)
 
 
-    override suspend fun textOtherwise(action: suspend () -> Unit) {
+    override suspend fun textOtherwise(action: suspend (textMsg: Text) -> Unit) {
         if (!matched && msg is Text) {
             require(msg is Text)
             execute(action)
@@ -46,17 +53,17 @@ data class ReplyHelperImpl(val callScope: BotScope, val inEvent: InEvent?) :
         }
     }
 
-    private suspend fun execute(action: suspend () -> Unit) {
-        action()
+    private suspend fun <E : InEvent> execute(action: suspend (E) -> Unit) {
+        action(msg as E)
         matched = true
     }
 
-    private suspend fun <T> execute(action: suspend (T) -> Unit, arg: T) {
-        action(arg)
+    private suspend fun <T, E : InEvent> execute(action: suspend (E, T) -> Unit, arg: T) {
+        action(msg as E, arg)
         matched = true
     }
 
-    override suspend fun <T> on(predicate: ReplyCondition<T>, action: suspend (T) -> Unit) {
+    override suspend fun <T, E : InEvent> on(predicate: ReplyCondition<T>, action: suspend (event: E, T) -> Unit) {
         if (!matched)
             predicate(msg)?.let { execute(action, it) }
     }
